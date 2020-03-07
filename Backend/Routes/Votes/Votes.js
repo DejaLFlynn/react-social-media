6367769d8cc44653a3e50a1b6a128002b71ffe0e
ashya <ashyamanning@pursuit.org>
2020-03-07 17:11:44 -0500

    create vote queries

--- Backend/Routes/Votes/Votes.js
+++ Backend/Routes/Votes/Votes.js
@@ -1,11 +1,11 @@
 const votes = require("express").Router()
-const {createVote, getVote} = require("../../Queries/VotesQueries")
+const { getVote } = require("../../Queries/VotesQueries")
 
 
-const userVotesRouter = require("../Images") 
-votes.use('/', userVotesRouter);
+// const userVotesRouter = require("../Images") 
+// votes.use('/', userVotesRouter);
 
-votes.post("/", createVote)
+// votes.post("/", createVote)
 
 
 votes.get("/votes", getVote)
