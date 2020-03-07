be67576cf2f00ee4531269f377002db1e63dcdf3
Nilber Remon <nilberremon@gmail.com>
2020-03-07 15:29:43 -0500

    Add Form Component

---
+++ Frontend/click/src/Components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Input from './Input'
+
+const Form = (props) => {
+    
+    const handleSubmit = () => {
+        
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+
+        </form>
+
+    )
+}
+
+export default Input
