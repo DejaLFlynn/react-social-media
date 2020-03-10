d4141c74e0db214a8fcadcdc44cfa3ea0da8cc0e
Nilber Remon <nilberremon@gmail.com>
2020-03-10 19:17:38 -0400

    Debug  Server.js from Multer Code

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
 const multer = require('multer');
-const uploadPath = multer({ destination: '../Frontend/click/Assets/Images' });
-//app.use(express.static(__dirname + '/public'))
+const uploadPath = multer({ dest: '../Frontend/click/Assets/Images' });
+//app.use(express.static(__dirname + '/../Frontend/click/Assets'))
 
 
 const port = 4000;
