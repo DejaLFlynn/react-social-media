bd460b1a64d0d2599579d6ac14ffa9aceb6647fe
Nilber Remon <nilberremon@gmail.com>
2020-03-09 20:30:38 -0400

    useFetch Custom Hook

--- Frontend/click/src/App.js
+++ Frontend/click/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import './App.css';
-import SignUpPage from './Components/SignUpPage'
-import LandingPage from './Components/LandingPage'
-
 
 import Authorization from './Components/Authorization'
+
 
 const App = () => {
   return (
