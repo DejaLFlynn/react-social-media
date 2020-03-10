ae000f5756311e2f662ff03b1a1c94dceacc84df
karenm09 <karenm929@hotmail.com>
2020-03-09 21:10:51 -0400

    update the nestedHashtags.js route and queries

--- Backend/Routes/Images/Images.js
+++ Backend/Routes/Images/Images.js
@@ -3,8 +3,8 @@ const { createImage, deleteImage } = require('../../Queries/ImageQueries');
 
 const imageVotesRouter = require("./ImagesNestedRoutes/NestedVotes");
 const imageHashtagRouter = require("./ImagesNestedRoutes/NestedHashtags");
-images.use("/:id/votes", imageVotesRouter);
-images.use("/:id/hashtags", imageHashtagRouter);
+images.use("/votes", imageVotesRouter);
+images.use("/hashtags", imageHashtagRouter);
 
 
 images.delete("/:id", deleteImage);
