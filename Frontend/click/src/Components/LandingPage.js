8573b9198190f3e49b85627b6ebfc029e1a1b579
ashya <ashyamanning@pursuit.org>
2020-03-10 21:54:59 -0400

    styling header

--- Frontend/click/src/Components/LandingPage.js
+++ Frontend/click/src/Components/LandingPage.js
@@ -1,100 +1,96 @@
-import React, {useState, useEffect} from 'react'
-import axios from "axios"
-import {Link} from 'react-router-dom'
-import { useInput } from '../Utilities/CustomHooks'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useInput } from "../Utilities/CustomHooks";
 
-import Input from './Input'
-import Error from './Error'
+import Input from "./Input";
+import Error from "./Error";
 
-import '../CSS/LandingPage/style.css'
+import "../CSS/LandingPage/style.css";
 
+const LandingPage = ({ onLogin }) => {
+  const username = useInput("");
+  const email = useInput("");
+  const [error, setError] = useState("");
+  const [images, setImages] = useState("");
 
-const LandingPage = ({onLogin}) => {
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      let res = await axios.get(
+        `http://localhost:4000/users/${username.value}`
+      );
 
-      const username = useInput("")
-      const email = useInput("")
-      const [error, setError] = useState("")
-      const [images, setImages] = useState("")
-
-    
-    const handleSubmit = async (event)=>{
-      event.preventDefault()
-      try{
-        let res = await axios.get(`http://localhost:4000/users/${username.value}`)
-    
-        if (res) {
-          sessionStorage.setItem("username", username.value);
-          sessionStorage.setItem("id", res.data.payload.id)
-          onLogin()
-        }
-      }catch(error){
-        setError("Please Enter a Valid Username or Sign the F*ck Up")
+      if (res) {
+        sessionStorage.setItem("username", username.value);
+        sessionStorage.setItem("id", res.data.payload.id);
+        onLogin();
       }
-
-        
+    } catch (error) {
+      setError("Please Enter a Valid Username or Sign the F*ck Up");
     }
-    const getTopPic = async()=>{
-        const imageUrl = `http://localhost:4000/votes`
-      
-        try{
-            let res = await axios.get(imageUrl);
-            setImages(res.data.payload[0].picture);
-        }catch(error){
-            setImages("")
-        }
+  };
+  const getTopPic = async () => {
+    const imageUrl = `http://localhost:4000/votes`;
+
+    try {
+      let res = await axios.get(imageUrl);
+      setImages(res.data.payload[0].picture);
+    } catch (error) {
+      setImages("");
     }
-  
-    useEffect(()=>{
+  };
 
-            getTopPic()
-   
-    },[])
-   
+  useEffect(() => {
+    getTopPic();
+  }, []);
 
-     
-    return (
-        <>
-
-          <div className="LandingPage">
-    
-            <div className="Container">
-              <div className="header">
-                Logo
-              </div>
-              
-              <div className="popularPic">
-              <img src={images} alt={""}  className="topPic"/>
-              </div>
-              
-              <div className="signIn"> 
-                <form onSubmit={handleSubmit}>
-
-                    <Input className="userInputs" placeholder={"Enter Username"} input={username}/>
-                  
-                  <br></br>
-  
-                    <Input className="userInputs" placeholder={"Enter Email"} input={email}/>  
-                
-                  <br></br>
-                  
-                  <button className="signInBtn" type="submit">Sign In</button>
-                  
-                  <br></br>
-                  
-                  <Link to={'/SignUp'} className="signUpLink">Sign Up</Link>
-                
-                </form>
-
-              </div>
-
-            </div>
-            <div>
-            { !error ? <Error message={error}/> : null}
-            </div>
+  return (
+    <>
+      <div className="LandingPage">
+        <div className="Container">
+          <div className="header">
+            <span className="LogoLetter">C</span>lick me til I scream!
           </div>
 
-        </>
-    )
-}
+          <div className="popularPic">
+            <img src={images} alt={""} className="topPic" />
+          </div>
+
+          <div className="signIn">
+            <form onSubmit={handleSubmit}>
+              <Input
+                className="userInputs"
+                placeholder={"Enter Username"}
+                input={username}
+              />
+
+              <br></br>
+
+              <Input
+                className="userInputs"
+                placeholder={"Enter Email"}
+                input={email}
+              />
+
+              <br></br>
+
+              <button className="signInBtn" type="submit">
+                Sign In
+              </button>
+
+              <br></br>
+
+              <Link to={"/SignUp"} className="signUpLink">
+                Sign Up
+              </Link>
+            </form>
+          </div>
+        </div>
+        <div>{!error ? <Error message={error} /> : null}</div>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
