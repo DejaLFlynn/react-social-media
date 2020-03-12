2366e7175e6e7c9657da561abe669eb0ea03af9f
ashya <ashyamanning@pursuit.org>
2020-03-11 22:04:48 -0400

    add background to homepage

--- Frontend/click/src/Components/Home.js
+++ Frontend/click/src/Components/Home.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ImageUpload from './ImageUpload'
-import Image from './Image'
+import React from "react";
+import ImageUpload from "./ImageUpload";
+import Image from "./Image";
+import "../CSS/HomePage.css";
 
 const Home = () => {
   return (
     <div className="Home">
-
       <ImageUpload />
-    
     </div>
   );
-}
+};
 
 export default Home;
