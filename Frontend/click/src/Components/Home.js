5d6668488d19ea99d4763e28bddaa2f9ef458ecb
ashya <ashyamanning@pursuit.org>
2020-03-14 18:48:23 -0400

    styling homepage

--- Frontend/click/src/Components/Home.js
+++ Frontend/click/src/Components/Home.js
@@ -1,42 +1,42 @@
-import React, {useState, useEffect } from 'react';
-import ImageUpload from './ImageUpload';
-import Image from './Image'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import ImageUpload from "./ImageUpload";
+import Image from "./Image";
+import axios from "axios";
+import "../CSS/HomePage.css";
 
 const Home = () => {
-  const [randPics, setRandPics]= useState([]);
-  
+  const [randPics, setRandPics] = useState([]);
 
-  const getRandPics = async ()=> {
-    const randPicsUrl = `http://localhost:4000/images/hashtags/`
-    try{
-      let res = await axios.get(randPicsUrl)
-      setRandPics(res.data.payload)
-
-    }catch(err){
-      setRandPics([])
+  const getRandPics = async () => {
+    const randPicsUrl = `http://localhost:4000/images/hashtags/`;
+    try {
+      let res = await axios.get(randPicsUrl);
+      setRandPics(res.data.payload);
+    } catch (err) {
+      setRandPics([]);
     }
-  }
+  };
 
-  useEffect(()=>{
-    getRandPics()   
-}, []);
+  useEffect(() => {
+    getRandPics();
+  }, []);
 
-let displayRandPics = randPics.map(pic =>{
-  return <li className="randPic" key={pic.id}><Image image={pic.picture}/> 
-    {pic.user_hashtags.map((tag, idx)=>(
-      <p className="tags" key={idx} >#{tag}</p>
-    ))}
-    </li>
-})
-
+  let displayRandPics = randPics.map(pic => {
+    return (
+      <div className="imageContainer" key={pic.id}>
+        <Image className="randPic" image={pic.picture} />
+        {pic.user_hashtags.map((tag, idx) => (
+          <p className="tags" key={idx}>
+            #{tag}
+          </p>
+        ))}
+      </div>
+    );
+  });
 
   return (
     <div className="Home">
-      
-    <ul className="randomPics">
-      {displayRandPics}
-    </ul>
+      <div className="randomPics">{displayRandPics}</div>
     </div>
   );
 };
