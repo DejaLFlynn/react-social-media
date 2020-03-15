015d971bfcf3ea46f8228bd1fc0f500928ce37ca
Nilber Remon <nilberremon@gmail.com>
2020-03-14 20:26:44 -0400

    Profile Background

--- Frontend/click/src/Components/ProfilePage.js
+++ Frontend/click/src/Components/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from "axios"
 import Image from "./Image"
-import '../CSS/LandingPage/profilePage.css'
+import '../CSS/profilePage.css'
 
 
 const Profile = () => {
