930eb788110844a23b048ee53ca91e07e5be1f8b
ashya <ashyamanning@pursuit.org>
2020-03-15 13:14:21 -0400

    image upload on modal

--- Backend/server.js
+++ Backend/server.js
@@ -5,78 +5,76 @@ const bodyParser = require("body-parser");
 const port = 4000;
 
 const app = express();
-const multer = require('multer');
+const multer = require("multer");
 const path = require("path");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true,
-    limit: '50mb',
+    limit: "50mb",
     parameterLimit: 50000
-}));
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(express.static(path.join(__dirname, 'Public')))
+  })
+);
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "Public")));
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './Backend/Public/Uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "./Backend/Public/Uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || filetype.mimetype === 'image/png' || filetype.mimetype === 'image/gif') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
+  if (
+    file.mimetype === "image/jpeg" ||
+    filetype.mimetype === "image/png" ||
+    filetype.mimetype === "image/gif"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5 //bytes 1mb = 1024 bytes
-    },
-    //fileFilter: fileFilter
-})
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 //bytes 1mb = 1024 bytes
+  }
+  //fileFilter: fileFilter
+});
 
+app.post("/upload", upload.single("image"), (req, res, next) => {
+  try {
+    console.log(req.file);
+    let url = "http://localhost:4000/Uploads/" + req.file.filename;
+    res.json({
+      imageUrl: url,
+      message: "File Uploaded"
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: error,
+      message: "Upload Failed"
+    });
+  }
+});
 
-app.post('/upload', upload.single("image"), (req, res, next) => {
-    try {
-        debugger
-        let url = "http://localhost:4000/Uploads/" + req.file.filename
-        res.json({
-            imageUrl: url,
-            message: "File Uploaded"
-        })
+const usersRouter = require("./Routes/Users/Users");
+const imagesRouter = require("./Routes/Images/Images");
+const votesRouter = require("./Routes/Votes/Votes");
+const hashtagsRouter = require("./Routes/Hashtags/Hashtags");
 
-    } catch (error) {
-        res.status(400).json({
-            status: error,
-            message: "Upload Failed"
-        })
-    }
-})
+app.use("/users", usersRouter);
+app.use("/images", imagesRouter);
+app.use("/votes", votesRouter);
+app.use("/hashtags", hashtagsRouter);
 
-
-const usersRouter = require("./Routes/Users/Users")
-const imagesRouter = require("./Routes/Images/Images")
-const votesRouter = require("./Routes/Votes/Votes")
-const hashtagsRouter = require("./Routes/Hashtags/Hashtags")
-
-
-app.use("/users", usersRouter)
-app.use("/images", imagesRouter)
-app.use("/votes", votesRouter)
-app.use("/hashtags", hashtagsRouter)
-
-app.listen(port, () => console.log(`Server running on Port: ${port}`))
-
-
-
-
+app.listen(port, () => console.log(`Server running on Port: ${port}`));
 
 // {
 //     fieldname: 'image',
--- Frontend/click/src/Components/ImageUpload.js
+++ Frontend/click/src/Components/ImageUpload.js
@@ -1,64 +1,85 @@
-import React, { useState } from 'react'
-import { useInput } from '../Utilities/CustomHooks'
-import axios from 'axios'
-import Input from './Input'
-import '../CSS/ImageUpload.css'
+import React, { useState } from "react";
+import { useInput } from "../Utilities/CustomHooks";
+import axios from "axios";
+import Input from "./Input";
+import "../CSS/ImageUpload.css";
 
-const ImageUpload = ({modalClose}) => {
-    
-    const [image, setImage] = useState(null)
-    const hashtag = useInput("")
+const ImageUpload = ({ modalClose }) => {
+  const [image, setImage] = useState(null);
+  const hashtag = useInput("");
 
-    const handleUpload = (event) => {
-        setImage(event.target.files[0])
+  const handleUpload = event => {
+    setImage(event.target.files[0]);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    event.target.hashtag.value = null; //Better Way of Coding?
+    event.target.image.value = null;
+    const formData = new FormData();
+    formData.append("image", image);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+    try {
+      let res = await axios.post(
+        "http://localhost:4000/upload",
+        formData,
+        config
+      );
+      let imageUrl = res.data.imageUrl;
+      let resImage = await axios.post(
+        `http://localhost:4000/users/${sessionStorage.id}/images`,
+        {
+          image: imageUrl
+        }
+      );
+      let imageId = resImage.data.payload.id;
+      let resHashtag = await axios.post(
+        "http://localhost:4000/images/hashtags",
+        {
+          picture_id: imageId,
+          body: hashtag.value
+        }
+      );
+      modalClose();
+    } catch (error) {
+      //errorAlert = "Image Failed to Upload"
     }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        event.target.hashtag.value = null //Better Way of Coding?
-        event.target.image.value = null
-        const formData = new FormData()
-        formData.append("image", image)
-        const config = {
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        }
-        try { 
-            let res = await axios.post('http://localhost:4000/upload', formData, config)
-            let imageUrl = res.data.imageUrl
-            let resImage = await axios.post(`http://localhost:4000/users/${sessionStorage.id}/images`, {
-                image: imageUrl
-            })
-            let imageId = resImage.data.payload.id
-            let resHashtag = await axios.post('http://localhost:4000/images/hashtags', {
-                picture_id: imageId,
-                body: hashtag.value
-            })
-            modalClose()
-        } catch (error) {
-            //errorAlert = "Image Failed to Upload"
-        }
-     }
-
-    return(
-        <div className="uploadContainer">
-            <form className="uploadForm" onSubmit={handleSubmit}>
-                <div className="uploadImage">
-                    <p>Choose Your Comedic Gold</p>
-                    <input className="uploadImage" type="file" name="image" onChange={(event) => handleUpload(event)}/>
-                </div>
-                <div className="inputContainer">
-                    <Input className="input" type={"text"} name={'hashtag'} placeholder={'Enter Hashtags w/ Spaces'} input={hashtag}/>
-                </div>
-                <div className="submit">
-                    <button className="submitButton" type='submit'>UPLOAD</button>
-                    <p>MAKE US CRY LAUGHING</p>
-                </div>
-            </form>
+  return (
+    <div className="uploadContainer">
+      <form className="uploadForm" onSubmit={handleSubmit}>
+        <div className="uploadImage">
+          <p>Choose Your Comedic Gold</p>
+          <input
+            className="uploadImage"
+            type="file"
+            name="image"
+            onChange={event => handleUpload(event)}
+          />
         </div>
-    )       
-}
+        <div className="inputContainer">
+          <Input
+            className="input"
+            type={"text"}
+            name={"hashtag"}
+            placeholder={"Enter Hashtags w/ Spaces"}
+            input={hashtag}
+          />
+        </div>
+        <div className="submit">
+          <button className="submitButton" type="submit">
+            UPLOAD
+          </button>
+          <p>MAKE US CRY LAUGHING</p>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default ImageUpload
-
+export default ImageUpload;
